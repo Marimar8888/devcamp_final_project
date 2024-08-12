57cb7821d16a2e5756d9be53b72145373bf59bda
Marimar <malonso@quadram.mobi>
2024-08-12 17:53:48 +0200

    login complete

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 import os
 
 # Inicializar extensiones
@@ -16,6 +17,8 @@ def create_app():
     # Inicializar extensiones
     db.init_app(app)
     ma.init_app(app)
+
+    CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
     
     # Registrar Blueprints
     from app.routes import bp
